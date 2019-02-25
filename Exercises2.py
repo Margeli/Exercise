c4932e4c85a241d0952ca569b730fa1ae2b65956
Margeli <niniopantalones@gmail.com>
2019-02-25 10:28:23 +0100

    ss

--- Exercises2.py
+++ Exercises2.py
@@ -4,8 +4,12 @@ import cv2
 
 
 def Ex1():
-    img = cv2
+    img = cv2.imread("image.png")
 
+    imgInfo = n.dtype
+    print(img.size())
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
